2a44f098b2ecbc1fcab7a7b89e7b8791fff0da69
JJKOrlino <jjkorlino80@yahoo.com>
2020-12-02 08:28:12 -0600

    commit tree 8db6b87dc51170acf0cb5887a9aa0dbb6820529b

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,22 +1,42 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
+
 
 require_relative './course.rb'
 
+
 class Scraper
-  
+  	
   def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
+    self.make_courses	
+    Course.all.each do |course|	
+
       end
     end
+  end	
+
+
+  def get_page
+
+    Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
   end
-  
+
+  def get_courses
+    self.get_page.css(".post")
+  end
+
+  def make_courses
+    self.get_courses.each do |post|
+      course = Course.new
+      course.title = post.css("h2").text.strip
+      course.schedule = post.css(".date").text.strip
+      course.description = post.css("p").text.strip
+    end
+  end
+
 end
 
 
-
+Scraper.new.print_courses
